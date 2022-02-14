92c3c6bd09038d98752ee5aa5f08d72d5e102f64
Bean1999 <97262220+Bean1999@users.noreply.github.com>
2022-02-14 20:51:35 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "Hông! Đá cho 1 cái.",
   titleModar: "Cảm ơn em vì đã huông anhh",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Anh biết là bé huông anh màaa ^^ </br> Cám ơn em đã đồng ý để cho được bên em. Đối với anh, em mãi là cô bé :)) </br> "huông em nhìu nhìu nhìuuu. </br>  Lắm lắm lắmmm "',
 };
